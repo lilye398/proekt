256f0a7d3d086f42374a6b3ee27ec9dd8f3ad101
lilye398 <156074322+lilye398@users.noreply.github.com>
2024-04-25 21:12:55 +0300

    Add files via upload

---
+++ проект.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+#БД - Таблицы - Записи
+#Таблица:
+# id  title   price   isActive
+# 1   Some    100     True
+# 2   Some2   200     False
+# 3   Some3   40      True
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    isActive = db.Column(db.Boolean, default=True)
+    text = db.Column(db.Text, nullable=False)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/create')
+def create():
+    return render_template('create.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
